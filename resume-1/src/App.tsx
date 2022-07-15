e86b5ea990668f6db572cc4c35b5da841d27a015
Akshat Sharma <akshatsharma3800@gmail.com>
2022-07-15 19:59:06 +0530

    - export button

--- resume-1/src/App.tsx
+++ resume-1/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DetailSection from './components/detail';
 import MasterSection from './components/master';
 import './styles.css';
+import { Button } from '@innovaccer/design-system';
 
 const App = () => {
 	return (
@@ -13,6 +14,9 @@ const App = () => {
 				<div className="detail-section">
 					<DetailSection />
 				</div>
+			</div>
+			<div className="export-button-container p-6">
+				<Button>Export</Button>
 			</div>
 		</div>
 	);
