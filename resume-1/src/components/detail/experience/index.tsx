c737718e2d68c2c65a9e503171339121120a481c
Akshat Sharma <akshatsharma3800@gmail.com>
2022-07-16 22:24:29 +0530

    - added info about role

--- resume-1/src/components/detail/experience/index.tsx
+++ resume-1/src/components/detail/experience/index.tsx
@@ -13,7 +13,7 @@ const ExperienceSection: React.FunctionComponent<IExperienceSectionProps> = (pro
 					<DetailItem
 						startYear="2020"
 						title="Senior Software Engineer"
-						caption="Innovaccer Inc., Noida"
+						caption="Innovaccer Inc., Noida (Healthcare)"
 						description="Worked for client like Roche on the product AIMS as a full stack developer. Currently performing all duties of Team Lead, and contributing to frontend, backend, mobile and DevOps.
 						Leading Innovaccer's open source mobile design system team and managing the efforts of the mobile team. Having ownership of all the apps on iOS platform in the company and frontend of Roche. Have single handedly made CI/CD pipeline for mobile apps. Created custom dart-pad and dart-services for developers to try out design system. Have also worked on projects like CBO, Patient App and InNote. Tech stack currently being used is react, django and flutter."
 					/>
@@ -23,7 +23,7 @@ const ExperienceSection: React.FunctionComponent<IExperienceSectionProps> = (pro
 						startYear="2018"
 						endYear="2020"
 						title="Software Engineer"
-						caption="Myntra-Jabong, Gurgaon"
+						caption="Myntra-Jabong, Gurgaon (E-commerce)"
 						description="Worked on Jabong's native iOS mobile app for initial year and then contributed to Myntra's mobile app which was written on top of React native. Implemented rich notifications, seamless login and Myntra's category tab. Automated Jabong's iOS app using Jenkins and Fastlane. Tech stack used was Swift, Objective C, React native, JS and GoLang."
 					/>
 				</div>
